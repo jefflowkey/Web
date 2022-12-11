d7bf982816a206fcc9608b3f5c5e5640864db3f3
jefflowkey <munyao.v.jm@gmail.com>
2022-12-11 18:04:08 +0300

    Added JS code to main.js & sorted contact textarea

--- main.js
+++ main.js
@@ -41,3 +41,48 @@ function submitted(event) {
 
 // Listener -> triggered by clicking the submit button then runs the function above
 f.addEventListener('submit', submitted);
+
+// Sidebar script
+
+let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", (e) => {
+            let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+                arrowParent.classList.toggle("showMenu");
+            });
+            }
+        
+        let sidebar = document.querySelector(".sidebar");
+        let sidebarBtn = document.querySelector(".bx-menu");
+        console.log(sidebarBtn);
+
+        /* sidebarBtn.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+        }); */
+            
+        $(function () {
+        /* console.log("width: "+ document.body.clientWidth); */
+            
+        resizeScreen();
+        $(window).resize(function(){
+            resizeScreen();
+        });
+        $('.bx-menu').click(function(){
+        
+        //.close，.small-screen .close，
+        if(document.body.clientWidth > 400){
+        $('.sidebar').toggleClass('close');
+        }else{
+        $('.sidebar').toggleClass('small-screen');
+        }
+        });
+            
+        function resizeScreen() {
+        // .categoriesclose，.categories.close
+            if(document.body.clientWidth < 400){
+                $('.sidebar').addClass('close');
+            }else{
+                $('.sidebar').removeClass('close');
+            }
+            }
+            });
